7cd924dd34d6e0a5a3f592c0d007dfc47dec53ee
Kristian Neely <32805776+kneely13@users.noreply.github.com>
2018-08-25 20:24:01 -0400

    Just need to add images and fix contaiers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,11 @@ var lost =0;
 var win = 0;
 var total = 0;
 
-var startGame = function () {
+
+
+var resetAndStartGame = function () {
+
+    $(".crystalsContainer").empty();
 
     random_number = Math.floor(Math.random() * 99) +19;
 
@@ -12,7 +16,8 @@ var startGame = function () {
     for(var i=0; i<4; i++){
 
         var random = Math.floor(Math.random()*12) + 1;
-        //console.log(random);
+        console.log(random);
+
 
         var crystal = $("<div>");
             crystal.attr({
@@ -20,30 +25,40 @@ var startGame = function () {
                 "data-random": random
             });
 
+         
+
         $(".crystalsContainer").append(crystal);
     }
+
+    $(".scoreCounter").html(total);
 }
 
-startGame();
+resetAndStartGame();
 
 
-
-$(".crystal").on('click', function () {
+$(document).on('click', ".crystal",function () {
 
     var crystalNumber = parseInt($(this).attr('data-random'));
+    
     total += crystalNumber;
-    $(".scoreCounter").text(total);
-    console.log(total);
+    
+    $('.scoreCounter').html(total);
+    console.log(total)
 
     if(total > random_number){
         lost++;
-        $('#lossCounter').text('');
+        $('#lost').html('Losses: ' + lost);
+        total=0;
+        resetAndStartGame();
+
     }
     else if(total === random_number ){
         win ++;
-        $('#winCounter').text();
+        $('#win').html('Wins: ' + win);
+        total=0;
+        resetAndStartGame();
     }
-});
+});// 
 
 
 
@@ -52,8 +67,11 @@ $(document).ready(function() {
     $('.titleContainer').append("Crystals Collector");
 
 
-    $('.totalContainer').append("Your total score is: ");
+    // $('#win').text("Wins: " + win);
+    // $('#lost').text( "Losses: " + lost);
 
+    $('.totalContainer').append("Your total score is: ");
+    
     $(".bg").css("background-image", "url('/css/images/css.jpg')");
    
 }); //end of method
